ee41d200940c870c74517d72c673708391a7ce7f
Heidi Merianne <hmeriann@f1r3s13.codam.nl>
2023-09-08 11:04:11 +0200

    blanck project with no storyboard

---
+++ ft_hangouts/ft_hangouts/AppDelegate.swift
@@ -0,0 +1,60 @@
+//
+//  AppDelegate.swift
+//  ft_hangouts
+//
+//  Created by Heidi Merianne on 9/8/23.
+//  Copyright © 2023 Zuleykha Pavlichenkova. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let window = UIWindow()
+        let viewController = UIViewController()
+        
+        viewController.view.backgroundColor = .white
+        
+        window.rootViewController = viewController
+        window.makeKeyAndVisible()
+        self.window = window
+        return true
+    }
+
+
+    // MARK: - Core Data stack
+
+    lazy var persistentContainer: NSPersistentContainer = {
+    
+        let container = NSPersistentContainer(name: "ft_hangouts")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+
+    // MARK: - Core Data Saving support
+
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+               
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+
+}
+
---
+++ ft_hangouts/ft_hangouts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ft_hangouts
+//
+//  Created by Heidi Merianne on 9/8/23.
+//  Copyright © 2023 Zuleykha Pavlichenkova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
