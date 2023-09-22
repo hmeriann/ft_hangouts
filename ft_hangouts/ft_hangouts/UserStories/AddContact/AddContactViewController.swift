2775dfcf0fc2fc14c6023e0a046790a0629ca920
Zuleykha Pavlichenkova <zuleykha.pavlichenkova@gmail.com>
2023-09-22 13:16:23 +0200

    tuned the Add view to be able to type in the fields and added the date picker

--- ft_hangouts/ft_hangouts/UserStories/AddContact/AddContactViewController.swift
+++ ft_hangouts/ft_hangouts/UserStories/AddContact/AddContactViewController.swift
@@ -12,6 +12,19 @@ final class AddContactViewController: UIViewController {
     
     let contacts = Contact.defaultContacts()
     
+    private lazy var scrollView: UIScrollView = {
+        let view = UIScrollView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private lazy var scrollContentView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .quaternarySystemFill
+        return view
+    }()
+    
     private lazy var userPicure: UIImageView = {
         let userpic = UIImageView()
         userpic.translatesAutoresizingMaskIntoConstraints = false
@@ -23,62 +36,138 @@ final class AddContactViewController: UIViewController {
         return userpic
     }()
     
-    private lazy var tableView: UITableView = {
-        let table = UITableView(frame: view.bounds, style: .grouped)
-        table.translatesAutoresizingMaskIntoConstraints = false
-        table.register(AddContactCell.self, forCellReuseIdentifier: "addContactCell")
-        table.dataSource = self
-        table.delegate = self
-        return table
+    private lazy var nameField: UITextField = {
+        let field = UITextField()
+        field.translatesAutoresizingMaskIntoConstraints = false
+        field.font = .systemFont(ofSize: 24)
+        field.borderStyle = .roundedRect
+        field.placeholder = "First name"
+        field.clearButtonMode = .whileEditing
+        return field
     }()
+    
+    private lazy var lastNameField: UITextField = {
+        let field = UITextField()
+        field.translatesAutoresizingMaskIntoConstraints = false
+        field.font = .systemFont(ofSize: 24)
+        field.borderStyle = .roundedRect
+        field.placeholder = "Last name"
+        field.clearButtonMode = .whileEditing
+
+        return field
+    }()
+    
+    private lazy var phoneNumberField: UITextField = {
+        let field = UITextField()
+        field.translatesAutoresizingMaskIntoConstraints = false
+        field.font = .systemFont(ofSize: 24)
+        field.borderStyle = .roundedRect
+        field.placeholder = "+X XXX XXX XXXX"
+        field.clearButtonMode = .whileEditing
+
+        return field
+    }()
+    
+    private lazy var emailField: UITextField = {
+        let field = UITextField()
+        field.translatesAutoresizingMaskIntoConstraints = false
+        field.font = .systemFont(ofSize: 24)
+        field.borderStyle = .roundedRect
+        field.placeholder = "email@email.nl"
+        field.clearButtonMode = .whileEditing
+
+        return field
+    }()
+    
+    private lazy var birthDatePicker: UIDatePicker = {
+        let picker = UIDatePicker()
+        picker.translatesAutoresizingMaskIntoConstraints = false
+        picker.datePickerMode = .date
+        picker.preferredDatePickerStyle = .wheels
+        return picker
+    }()
+    
+    private lazy var birthDateLabel: UILabel = {
+        let pickerLabel = UILabel()
+        pickerLabel.translatesAutoresizingMaskIntoConstraints = false
+        pickerLabel.font = .systemFont(ofSize: 24)
+        pickerLabel.textColor = .systemGray
+        pickerLabel.text = "Birth date"
+        return pickerLabel
+    }()
+    
+    private lazy var verticalStack: UIStackView = {
+        let stack = UIStackView()
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.alignment = .center
+        stack.axis = .vertical
+        stack.distribution = .equalSpacing
+        stack.spacing = 8
+        return stack
+    }()
+    
+    
+    func setUpUI() {
+        
+        view.addSubview(scrollView)
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
+        
+        scrollView.addSubview(scrollContentView)
+        NSLayoutConstraint.activate([
+            scrollContentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            scrollContentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            scrollContentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            scrollContentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            scrollContentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            scrollContentView.heightAnchor.constraint(equalTo: scrollView.heightAnchor)
+            
+        ])
+        scrollContentView.addSubview(userPicure)
+        NSLayoutConstraint.activate([
+            userPicure.heightAnchor.constraint(equalToConstant: 150),
+            userPicure.widthAnchor.constraint(equalToConstant: 150),
+            userPicure.topAnchor.constraint(equalTo: scrollContentView.safeAreaLayoutGuide.topAnchor, constant: 8),
+            userPicure.centerXAnchor.constraint(equalTo: scrollContentView.safeAreaLayoutGuide.centerXAnchor),
+        ])
+        
+        scrollContentView.addSubview(verticalStack)
+        NSLayoutConstraint.activate([
+            verticalStack.topAnchor.constraint(equalTo: userPicure.bottomAnchor, constant: 16),
+            verticalStack.leadingAnchor.constraint(equalTo: scrollContentView.leadingAnchor, constant: 16),
+            verticalStack.trailingAnchor.constraint(equalTo: scrollContentView.trailingAnchor, constant: -16),
+            //            verticalStack.bottomAnchor.constraint(equalTo: scrollContentView.bottomAnchor),
+        ])
+        verticalStack.addArrangedSubview(nameField)
+        verticalStack.addArrangedSubview(lastNameField)
+        verticalStack.addArrangedSubview(phoneNumberField)
+        verticalStack.addArrangedSubview(emailField)
+        verticalStack.addArrangedSubview(birthDateLabel)
+        verticalStack.addArrangedSubview(birthDatePicker)
+        NSLayoutConstraint.activate([
+            nameField.widthAnchor.constraint(equalTo: scrollContentView.widthAnchor),
+            lastNameField.widthAnchor.constraint(equalTo: scrollContentView.widthAnchor),
+            phoneNumberField.widthAnchor.constraint(equalTo: scrollContentView.widthAnchor),
+            emailField.widthAnchor.constraint(equalTo: scrollContentView.widthAnchor),
+            birthDateLabel.widthAnchor.constraint(equalTo: scrollContentView.widthAnchor),
+            birthDatePicker.widthAnchor.constraint(equalTo: scrollContentView.widthAnchor),
+
+        ])
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Add New Contact"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(saveTapped))
         setUpUI()
     }
-    
-    func setUpUI() {
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(saveTapped))
-        view.addSubview(userPicure)
-        NSLayoutConstraint.activate([
-            userPicure.heightAnchor.constraint(equalToConstant: 150),
-            userPicure.widthAnchor.constraint(equalToConstant: 150),
-            userPicure.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8),
-            userPicure.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
-        ])
-        view.addSubview(tableView)
-        NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalTo: userPicure.bottomAnchor, constant: 16),
-            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 4),
-            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -4),
-            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -4),
-        ])
-    }
-    
     
     
     @objc func saveTapped() {
         
     }
-}
-
-extension AddContactViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return contacts.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "addContactCell", for: indexPath)
-//        cell.configure(with: contacts[indexPath.row])
-        
-        return cell
-    }
-    
-    
-    
-}
-
-extension AddContactViewController: UITableViewDelegate {
-    
 }
